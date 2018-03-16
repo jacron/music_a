14986a1e402f2ac43daa85a589e640af708c13e8
jhsm croonen <info@jcroonen.nl>
2018-03-17 00:05:36 +0100

    no message

--- website/static/js/socket.js
+++ website/static/js/socket.js
@@ -1,31 +1,31 @@
 'use strict';
 
-function ajaxPost(data, cb) {
-    const url = '/ajax/';
-    const headers = {
-        'X-CSRFToken': $('input[name=csrfmiddlewaretoken]').val()
-    };
-    $.ajax({
-        type: 'POST',
-        url: url,
-        data: data,
-        headers: headers,
-        dataType: 'json'
-    }).done(function(response){
-        console.log(response);
-        if (cb) {
-            cb(response);
-        }
-    }).fail(function(err) {
-        console.log(err);
-        if (cb) {
-            cb(err);
-        }
-    });
-}
+// function ajaxPost(data, cb) {
+//     const url = '/ajax/';
+//     const headers = {
+//         'X-CSRFToken': $('input[name=csrfmiddlewaretoken]').val()
+//     };
+//     $.ajax({
+//         type: 'POST',
+//         url: url,
+//         data: data,
+//         headers: headers,
+//         dataType: 'json'
+//     }).done(function(response){
+//         console.log(response);
+//         if (cb) {
+//             cb(response);
+//         }
+//     }).fail(function(err) {
+//         console.log(err);
+//         if (cb) {
+//             cb(err);
+//         }
+//     });
+// }
 
 $(function(){
-    var socket = new WebSocket('ws://' + window.location.host + '/chat/');
+    const socket = new WebSocket('ws://' + window.location.host + '/chat/');
     function addToLog(data) {
         const logger = document.getElementById('logger') || document.getElementById('album-logger');
         if (logger) {
@@ -56,10 +56,9 @@ $(function(){
     };
 
     socket.onmessage = function message(e) {
-        // console.log(e);
         addToLog(JSON.parse(e.data));
         if (e.data.mode === 'error' && e.data.id) {
-            var album = $('li.hyperlink[id=' + e.data.id + ']');
+            const album = $('li.hyperlink[id=' + e.data.id + ']');
             if (album.length) {
                 console.log('album', album);
                 album.addClass('error');
@@ -70,10 +69,5 @@ $(function(){
     if (socket.readyState === WebSocket.OPEN) {
         socket.onopen();
     }
-    $('.test').click(function(){
-        ajaxPost({
-            'cmd': 'test'
-        });
-    })
 
 });
